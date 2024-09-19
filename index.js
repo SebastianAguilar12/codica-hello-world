d78bc687952ba7b5f8370b061f62d507d23e75a7
SebastianAguilar12 <sebas09981@gmail.com>
2024-09-19 07:18:32 -0500

    import logic function and update main on package.json

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log("Hello, Códica!");
 import _ from "lodash";
 
 console.log(_.last(["one", "two"]));
+
+import getLocation from "./src/index.js";
+
+console.log("¿Cuál es tu ubicación?");
+const location = "South: 3228";
+
+const address = getLocation (location);
+console.log(`Tu dirección es ${address}`);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default function (location) {
+    const address = "Calle 17 Sur # 39 - 75";
+
+    return address;
+}
+
