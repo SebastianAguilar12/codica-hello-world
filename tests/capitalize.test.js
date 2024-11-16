527e3832c6e61ea936b140f6c6a11ec852db8fc7
SebastianAguilar12 <sebas09981@gmail.com>
2024-11-15 21:52:16 -0500

    updating assert test function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('¡La función no funciona correctamente!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('¡La función no funciona correctamente!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('¡La función no funciona correctamente!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+
+// if (capitalize('') !== '') {
+//   throw new Error('¡La función no funciona correctamente!');
+// }
+
+assert.equal(capitalize(''), '');
 
 console.log('¡Todas las pruebas han pasado!');
